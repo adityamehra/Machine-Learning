833f3ef41c268884429d7caa3de6faaade7fe52f
Aditya Mehra <mehraaditya71@gmail.com>
2017-05-08 18:19:16 -0600

    Adding new Project.

---
+++ ucberkeley/code/analysis.py
@@ -0,0 +1,86 @@
+from __future__ import division
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+import pandas as pd
+
+
+
+data = pd.read_csv("../data/berkeley.csv", header= 0, sep=",")
+
+
+countMaleAdmit = 0
+countMaleReject = 0
+countFemaleAdmit = 0
+countFemaleReject = 0
+
+for index, row in data.iterrows():
+    if row["Gender"] == "Male" and row["Admit"] == "Admitted":
+        countMaleAdmit += row["Freq"]
+    elif row["Gender"] == "Male" and row["Admit"] == "Rejected":
+        countMaleReject += row["Freq"]
+    elif row["Gender"] == "Female" and row["Admit"] == "Admitted":
+        countFemaleAdmit += row["Freq"]
+    elif row["Gender"] == "Female" and row["Admit"] == "Rejected":
+        countFemaleReject += row["Freq"]
+
+counts = np.array([[countMaleAdmit, countMaleReject, countFemaleAdmit, countFemaleReject]])
+
+data1 = pd.DataFrame(counts, columns=['Male Admit', 'Male Reject', 'Female Admit', 'Female Reject'])
+
+# data1.plot.bar()
+# data1.plot.pie(subplots = True, figsize=(8, 8))
+# plt.show()
+
+countMaleAdmits = { "A" : 0, "B" : 0, "C" : 0, "D" : 0, "E": 0, "F" : 0}
+countMaleRejects = { "A" : 0, "B" : 0, "C" : 0, "D" : 0, "E": 0, "F" : 0}
+countFemaleAdmits = { "A" : 0, "B" : 0, "C" : 0, "D" : 0, "E" :0, "F" : 0}
+countFemaleRejects = { "A" : 0, "B" : 0, "C" : 0, "D" : 0, "E" : 0, "F" : 0}
+
+depts = ["A", "B", "C", "D", "E", "F"]
+
+for index, row in data.iterrows():
+    for dept in depts:
+        if row["Gender"] == "Male" and row["Admit"] == "Admitted" and row["Dept"] == dept:
+            countMaleAdmits[dept] += row["Freq"]
+        elif row["Gender"] == "Male" and row["Admit"] == "Rejected" and row["Dept"] == dept:
+            countMaleRejects[dept] += row["Freq"]
+        elif row["Gender"] == "Female" and row["Admit"] == "Admitted" and row["Dept"] == dept:
+            countFemaleAdmits[dept] += row["Freq"]
+        elif row["Gender"] == "Female" and row["Admit"] == "Rejected" and row["Dept"] == dept:
+            countFemaleRejects[dept] += row["Freq"]
+
+deptCounts = []
+
+for dept in depts:
+    deptCount = [countMaleAdmits[dept], countMaleRejects[dept], countFemaleAdmits[dept], countFemaleRejects[dept]]
+    deptCounts.append(deptCount);
+
+deptCounts = np.array(deptCounts)
+print deptCounts.shape
+
+# data1 = pd.DataFrame(deptCounts, columns=['Male Admit', 'Male Reject', 'Female Admit', 'Female Reject'])
+# data1.plot.bar()
+# plt.show()
+
+deptCounts = []
+
+for dept in depts:
+    deptCount = [countMaleAdmits[dept] / (countMaleAdmits[dept] + countMaleRejects[dept]) * 100, countFemaleAdmits[dept]/(countFemaleAdmits[dept]+countFemaleRejects[dept]) * 100]
+    deptCounts.append(deptCount);
+
+data1 = pd.DataFrame(deptCounts, columns=['Male Admit', 'Female Admit'])
+ax = data1.plot(kind='bar',alpha=0.75, rot=0)
+ax.set_xticklabels(depts)
+plt.xlabel('Dept', fontsize=18)
+plt.ylabel(" Percentage Admitted", fontsize=16)
+plt.show()
+
+# print countMaleAdmits
+# print countMaleRejects
+# print countFemaleAdmits
+# print countFemaleAdmits["A"]
+# print countFemaleAdmits["A"] + countFemaleRejects["A"]
+# print countFemaleAdmits["A"] / (countFemaleAdmits["A"] + countFemaleRejects["A"])
+# print countFemaleRejects
